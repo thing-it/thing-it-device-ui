b8a97b452eb9b03d22d896a33ed78a55f6f6278d
marcgille <latakia42>
2018-02-01 17:43:54 +0100

    UI tweaks.

--- dist/thing-it-device-ui.js
+++ dist/thing-it-device-ui.js
@@ -368,6 +368,7 @@ angular.module('thing-it-device-ui')
         controller: function ($element) {
             const vm = this;
             const plugin = $($element[0].querySelector('.motion-sensor-plugin'));
+            const occupiedIcon = $(plugin.find('#occupiedIcon'));
 
             vm.state = {occupied: false, lastMotionTimestamp: null, ticksPerMinute: 0};
             vm.options = {showLastMotionTimestamp: true, showTicksPerMinute: true};
@@ -375,6 +376,11 @@ angular.module('thing-it-device-ui')
             vm.render = render;
 
             function render() {
+                if (vm.state.occupied) {
+                    occupiedIcon.addClass('motion');
+                } else {
+                    occupiedIcon.removeClass('motion');
+                }
             }
 
             this.$onChanges = function (changes) {
